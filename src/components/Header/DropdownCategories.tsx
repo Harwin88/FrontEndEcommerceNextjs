50da01dbd1f37d9136080444b98e08360d8c90ca
OONNIGH <harwingalvis88@gmail.com>
2025-03-19 23:28:42 -0500

    Implement a responsive tree category view.

--- src/components/Header/DropdownCategories.tsx
+++ src/components/Header/DropdownCategories.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import AccordionSkeleton from "./loader/menu_spoin"
+import AccordionSkeleton from "./loader/menu_spoin";
 
 interface Category {
   documentId: string;
@@ -31,7 +31,6 @@ const CategoryAccordion = ({
   onCategoryClick,
   activeAncestors,
 }: CategoryAccordionProps) => {
-  // Si la categoría actual es parte de la ruta activa, se expande por defecto.
   const [expanded, setExpanded] = useState(activeAncestors.includes(category.documentId));
   const children = categories.filter(
     (cat) => cat.parent?.documentId === category.documentId
@@ -66,7 +65,7 @@ const CategoryAccordion = ({
           >
             <svg
               className={`w-4 h-4 transform transition-transform ${
-                expanded ? "rotate-90" : ""
+                expanded ? "rotate-180" : ""
               }`}
               fill="none"
               stroke="currentColor"
@@ -76,7 +75,7 @@ const CategoryAccordion = ({
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M9 5l7 7-7 7"
+                d="M19 9l-7 7-7-7"
               />
             </svg>
           </button>
@@ -105,15 +104,11 @@ const DropdownCategories = ({ categories, stickyMenu }: DropdownProps) => {
   const activePath = usePathname();
   const dropdownRef = useRef<HTMLLIElement>(null);
 
-  // Filtramos las categorías raíz (sin padre)
   const rootCategories = categories?.filter((cat) => !cat.parent) || [];
-
-  // Determinar la categoría activa según la ruta actual.
   const activeCategory = categories.find(
     (cat) => `/category/${cat.slug}` === activePath
   );
 
-  // Recopilamos los id de los ancestros de la categoría activa.
   let activeAncestors: string[] = [];
   if (activeCategory) {
     let current = activeCategory;
@@ -140,22 +135,48 @@ const DropdownCategories = ({ categories, stickyMenu }: DropdownProps) => {
   const handleCategoryClick = () => {
     setDropdownOpen(false);
   };
-
+  const activePathCategory = usePathname();
   return (
-    <li ref={dropdownRef} className="dropdown-container group relative">
+    <li ref={dropdownRef} className="group relative before:w-0 before:h-[3px] before:bg-blue before:absolute before:left-0 before:top-0 before:rounded-b-[3px] before:ease-out before:duration-200 hover:before:w-full dropdown-container group relative">
+      
+      <a
+       onClick={() => setDropdownOpen((prev) => !prev)}
+        href="#"
+        className={`md:hidden p-2 text-gray-700 hover:text-blue text-custom-sm font-medium text-dark flex items-center gap-1.5 capitalize ${
+          stickyMenu ? "xl:py-4" : "xl:py-6"
+        } ${activePathCategory.includes("category") && "!text-blue"}`}
+      >
+       Productos
+       <svg
+          className={`fill-current cursor-pointer transform transition-transform ${dropdownOpen ? "rotate-180" : ""}`}
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M2.95363 5.67461C3.13334 5.46495 3.44899 5.44067 3.65866 5.62038L7.99993 9.34147L12.3412 5.62038C12.5509 5.44067 12.8665 5.46495 13.0462 5.67461C13.2259 5.88428 13.2017 6.19993 12.992 6.37964L8.32532 10.3796C8.13808 10.5401 7.86178 10.5401 7.67453 10.3796L3.00787 6.37964C2.7982 6.19993 2.77392 5.88428 2.95363 5.67461Z"
+            fill=""
+          />
+        </svg>
+      </a>
+
       <a
         href="#"
         onClick={(e) => {
           e.preventDefault();
           setDropdownOpen((prev) => !prev);
         }}
-        className={`hover:text-blue text-custom-sm font-medium flex items-center gap-1.5 capitalize ${
+        className={`hover:text-blue text-custom-sm font-medium flex items-center gap-1.5 capitalize hidden md:flex ${
           stickyMenu ? "xl:py-4" : "xl:py-6"
-        } ${activePath.includes("categorias") && "!text-blue"}`}
+        } ${activePath.includes("Productos") && "!text-blue"}`}
       >
-        Categorías
+        Productos
         <svg
-          className="fill-current cursor-pointer"
+          className={`fill-current cursor-pointer transform transition-transform ${dropdownOpen ? "rotate-180" : ""}`}
           width="16"
           height="16"
           viewBox="0 0 16 16"
@@ -172,7 +193,7 @@ const DropdownCategories = ({ categories, stickyMenu }: DropdownProps) => {
       </a>
 
       {dropdownOpen && (
-        <div className="absolute left-0 top-full bg-white shadow-lg rounded-lg mt-2 w-[800px]">
+        <div className="absolute left-0 top-full bg-white shadow-lg rounded-lg mt-2 w-full md:w-[800px] z-50 backdrop-blur-md">
           <div className="p-6 max-h-[70vh] overflow-y-auto">
             {rootCategories.length > 0 ? (
               <ul>
